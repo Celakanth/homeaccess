53f33272fd8fc67740f0d8c53753431cadfa028b
hoarec <hoarec@timeslive.co.za>
2017-09-21 11:43:51 +0100

    Added login template

--- default.py
+++ default.py
@@ -11,12 +11,20 @@ app = Flask(__name__, template_folder='templates', static_url_path='/static')
 @app.route("/")
 
 
+# map the inputs to the function blocks
+
 def main():
-    page = request.args.get('page')
-    if page != '':
-        return render_template(page)
-    if page == '':
+    page = request.args.get('id')
+    if page is not None:
+        if page == '0':
+           return render_template('index.html')
+        if page == '1':
+           return render_template('login.html')
+    if page is None:
         return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
+# define the function blocks
+
+
