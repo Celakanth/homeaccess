8240c10ff978e5be3178015c6ecf470f30ea5e5c
hoarec <hoarec@timeslive.co.za>
2017-09-26 21:08:09 +0100

    changing templates

--- default.py
+++ default.py
@@ -3,30 +3,53 @@
 import cgi
 import cgitb
 import sys, urllib
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, escape
+from subprocess import call
 import pymysql
 
-
+sys.setrecursionlimit(2000)
 
 app = Flask(__name__, template_folder='templates', static_url_path='/static')
+app.config["DEBUG"] = True
+# app.secret_key = "ismine"
+comments = []
 
-@app.route("/")
 
-
+@app.route("/", methods=["GET", "POST"])
 # map the inputs to the function blocks
 
 def main():
     page = request.args.get('id')
     if page is not None:
         if page == '0':
-           return render_template('index.html')
+            return render_template('index.html', comments=str(sys.getrecursionlimit()))
         if page == '1':
-           return render_template('login.html')
+            if request.method == "POST":
+                comments.append(request.form["demo-name"])
+                session["username"] = comments
+                if comments == "celakanth":
+                    return render_template("message.html")
+                if 'username' in session:
+                    comments.append(escape(session['username']))
+                if comments.count("celakanth") > 0:
+                    return render_template("message.html")
+            else:
+                return render_template('login.html')
     if page is None:
-        return render_template('index.html')
+        if 'username' in session:
+            if request.method == "POST":
+                comments.append(escape(session['username']))
+                message = request.form["message"]
+                call(["espeak", "-a", "50", message])
+                return render_template("message.html")
+            else:
+                return render_template("message.html")
+
+    return render_template('index.html')
+
+
+app.secret_key = "A0Zr98j/3yX R~XHH!jmN]LWX/,?RT"
 
 if __name__ == "__main__":
     app.run()
 # define the function blocks
-
-
